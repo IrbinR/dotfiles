c525cb4b1c2c0435944453d568bd4c469a165759
IrbinR <salazar.irbin@gmail.com>
2025-08-01 18:17:30 -0500

    Configuraciones modificadas de lazy y kitty, init principal

--- nvim/lua/core/keymaps.lua
+++
@@ -1,3 +0,0 @@
-require("core.keymaps.toggleterm")
-require("core.keymaps.conform")
-require("core.keymaps.neotree")
--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,19 +1,19 @@
 vim.o.clipboard = "unnamedplus" --Habilitar portapapeles
 local opt = vim.opt
 
--- Apariencia y manejo de archivos
+-- ─[ Apariencia y manejo de archivos ]────────────────────────────────
 opt.number = true
 opt.relativenumber = true
 opt.cursorline = true
 opt.termguicolors = true
 
--- Comportamiento de edicción
+-- ─[ Comportamiento de edicción ]─────────────────────────────────────
 opt.expandtab = true
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.smartindent = true
 
--- Búsqueda
+-- ─[ Búsqueda ]───────────────────────────────────────────────────────
 opt.ignorecase = true
 opt.smartcase = true
 --opt.incsearch = true
@@ -23,5 +23,6 @@ opt.smartcase = true
 opt.wrap = true
 opt.linebreak = true
 
--- Barra de estado y lineas de comandos
+-- ─[ Barra de estado y lineas de comandos ]───────────────────────────
 opt.showmode = false -- Muestra el modo actual(insertar, normal y visual), si usas lualine desactivalo
+print("Cargando core/options")
