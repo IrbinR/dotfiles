130c17cb2b02d016b42f8152db4c7251a0ba8576
IrbinR <salazar.irbin@gmail.com>
2025-08-11 18:33:05 -0500

    Config toggleTerm - reorganización de keymaps

--- nvim/lua/core/keymaps/plugins/toggleterm.lua
+++ nvim/lua/core/keymaps/plugins/toggleterm.lua
@@ -3,22 +3,42 @@ return {
 	-- ─[ Terminal flotante (por defecto) ]────────────────────────────────
 	{
 		"<leader>tf",
-		"<cmd>ToggleTerm direction=float<cr>",
+		"<cmd>1ToggleTerm direction=float<cr>",
 		desc = "Toggle terminal flotante",
 	},
 
 	-- ─[ Terminal horizontal (15 líneas de altura) ]──────────────────────
 	{
-		"<leader>th",
-		"<cmd>ToggleTerm direction=horizontal size=15<cr>",
-		desc = "Toggle terminal horizontal",
+		"<leader>h1",
+		"<cmd>2ToggleTerm direction=horizontal size=15<cr>",
+		desc = "Toggle terminal horizontal 1",
+	},
+	{
+		"<leader>h2",
+		"<cmd>3ToggleTerm direction=horizontal size=15<cr>",
+		desc = "Toggle terminal horizontal 2",
+	},
+	{
+		"<leader>h3",
+		"<cmd>4ToggleTerm direction=horizontal size=15<cr>",
+		desc = "Toggle terminal horizontal 3",
 	},
 
 	-- ─[ Terminal vertical (60 líneas de altura) ]──────────────────────
 	{
-		"<leader>tv",
-		"<cmd>ToggleTerm direction=vertical size=60<cr>",
-		desc = "Toggle terminal horizontal",
+		"<leader>v1",
+		"<cmd>5ToggleTerm direction=vertical size=60<cr>",
+		desc = "Toggle terminal vertical 1",
+	},
+	{
+		"<leader>v2",
+		"<cmd>6ToggleTerm direction=vertical size=60<cr>",
+		desc = "Toggle terminal vertical 2",
+	},
+	{
+		"<leader>v3",
+		"<cmd>7ToggleTerm direction=vertical size=60<cr>",
+		desc = "Toggle terminal vertical 3",
 	},
 
 	-- ─[ Mapeos dentro del terminal ]─────────────────────────────────────
@@ -26,8 +46,3 @@ return {
 
 	{ "<C-w>h", [[<C-\><C-n><C-w>h]], mode = "t", desc = "Moverse a ventana izquierda" }, -- Navegar entre splits
 }
-
--- Terminal vertical (40% del ancho)
---[[ map("n", "<leader>tv", function()
-	toggleterm.toggle(2, vim.o.columns * 0.5, nil, "vertical")
-end, { desc = "Toggle terminal vertical" }) ]]
