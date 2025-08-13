9622f6049cfaca49ebddc3735eb607481cd38dbd
IrbinR <salazar.irbin@gmail.com>
2025-08-12 23:28:12 -0500

    Cambiando keymap de todocomment

--- nvim/lua/core/keymaps/plugins/todoComments.lua
+++ nvim/lua/core/keymaps/plugins/todoComments.lua
@@ -1,4 +1,4 @@
 local activeTodoComment = require("utils.todoCommentActive")
 return {
-	{ "<leader>st", activeTodoComment, desc = "Buscar tareas pendientes" },
+	{ "<leader>tc", activeTodoComment, desc = "Buscar tareas pendientes" },
 }
